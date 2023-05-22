53fdba36dfc2659fa414fe3713db1d51247af807
deepu agrawal <deepu.agrawal@acsicorp.com>
2023-05-22 18:20:38 +0530

    Data file.

---
+++ src/components/data-dummy.js
@@ -0,0 +1,604 @@
+const products = [
+    {
+      ID: 1,
+      label: "Tree Data1",
+      data: [
+        {
+          label: "Custom Label 1",
+          ID: 0,
+        },
+        {
+          label: "Custom Label 2",
+          ID: 1,
+        },
+        {
+          label: "Custom Label 3",
+          ID: 2,
+        },
+        {
+          label: "Custom Label 4",
+          ID: 3,
+        },
+        {
+          label: "Abnormal Vital Signs",
+          ID: 4,
+        },
+        {
+          label: "Acute Coronary Syndrome",
+          ID: 5,
+        },
+        {
+          label: "Infection Cellulitis",
+          ID: 6,
+        },
+        {
+          label: "Syncope",
+          ID: 7,
+        },
+        {
+          label: "Acute Kidney Injury",
+          ID: 8,
+        },
+        {
+          label: "Congestive Heart Failure",
+          ID: 9,
+        },
+        {
+          label: "Chronic Kidney Disease",
+          ID: 10,
+        },
+        {
+          label: "Encephalopathy",
+          ID: 11,
+        },
+        {
+          label: "Sepsis",
+          ID: 12,
+        },
+        {
+          label: "Shock",
+          ID: 13,
+        },
+        {
+          label: "Acute Tubular Necrosis",
+          ID: 14,
+        },
+        {
+          label: "Depression",
+          ID: 15,
+        },
+        {
+          label: "CC",
+          ID: 16,
+        },
+        {
+          label: "MCC",
+          ID: 17,
+        },
+      ],
+    },
+    {
+      ID: 2,
+      label: "Tree Data2",
+      data: [
+        {
+          label: "Abnormal Vital Signs",
+          ID: 0,
+        },
+        {
+          label: "Acute Coronary Syndrome",
+          ID: 1,
+        },
+        {
+          label: "Infection Cellulitis",
+          ID: 2,
+        },
+        {
+          label: "Syncope",
+          ID: 3,
+        },
+      ],
+    },
+    {
+      ID: 3,
+      label: "Release - Oct'21",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'21-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'21-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'21-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'21-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'21-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'21-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'21-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'21-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'21-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'21-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'21-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'21-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'21-Option-13",
+        },
+      ],
+    },
+    {
+      ID: 4,
+      label: "Release - Oct'22",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'22-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'22-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'22-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'22-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'22-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'22-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'22-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'22-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'22-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'22-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'22-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'22-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'22-Option-13",
+        },
+      ],
+    },
+    {
+      ID: 5,
+      label: "Release - Oct'23",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'23-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'23-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'23-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'23-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'23-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'23-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'23-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'23-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'23-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'23-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'23-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'23-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'23-Option-13",
+        },
+      ],
+    },
+    {
+      ID: 6,
+      label: "Release - Oct'24",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'24-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'24-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'24-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'24-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'24-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'24-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'24-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'24-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'24-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'24-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'24-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'24-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'24-Option-13",
+        },
+      ],
+    },
+    {
+      ID: 7,
+      label: "Release - Oct'25",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'25-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'25-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'25-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'25-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'25-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'25-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'25-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'25-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'25-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'25-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'25-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'25-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'25-Option-13",
+        },
+      ],
+    },
+    {
+      ID: 8,
+      label: "Release - Oct'26",
+      data: [
+        {
+          ID: 1,
+          label: "Release - Oct'26-Option-1",
+        },
+        {
+          ID: 2,
+          label: "Release - Oct'26-Option-2",
+        },
+        {
+          ID: 3,
+          label: "Release - Oct'26-Option-3",
+        },
+        {
+          ID: 4,
+          label: "Release - Oct'26-Option-4",
+        },
+        {
+          ID: 5,
+          label: "Release - Oct'26-Option-5",
+        },
+        {
+          ID: 6,
+          label: "Release - Oct'26-Option-6",
+        },
+        {
+          ID: 7,
+          label: "Release - Oct'26-Option-7",
+        },
+        {
+          ID: 8,
+          label: "Release - Oct'26-Option-8",
+        },
+        {
+          ID: 9,
+          label: "Release - Oct'26-Option-9",
+        },
+        {
+          ID: 10,
+          label: "Release - Oct'26-Option-10",
+        },
+        {
+          ID: 11,
+          label: "Release - Oct'26-Option-11",
+        },
+        {
+          ID: 12,
+          label: "Release - Oct'26-Option-12",
+        },
+        {
+          ID: 13,
+          label: "Release - Oct'26-Option-13",
+        },
+      ],
+    },
+  ];
+  
+  export const data = [
+    {
+      note: "Lorem ipsum dolor sit amet",
+      NotetypeID: 1,
+      createdBy: "John Doeed",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Consectetur adipiscing elit",
+      NotetypeID: 2,
+      createdBy: "Jane Smith",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Sed do eiusmod tempor incididunt",
+      NotetypeID: 2,
+      createdBy: "Bob Johnson",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Ut labore et dolore magna aliqua",
+      NotetypeID: 2,
+      createdBy: "Alice Brown",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Lorem ipsum dolor sit amet",
+      NotetypeID: 1,
+      createdBy: "John Doeed",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Consectetur adipiscing elit",
+      NotetypeID: 2,
+      createdBy: "Jane Smith",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Sed do eiusmod tempor incididunt",
+      NotetypeID: 2,
+      createdBy: "Bob Johnson",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Ut labore et dolore magna aliqua",
+      NotetypeID: 2,
+      createdBy: "Alice Brown",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Lorem ipsum dolor sit amet",
+      NotetypeID: 1,
+      createdBy: "John Doeed",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Consectetur adipiscing elit",
+      NotetypeID: 2,
+      createdBy: "Jane Smith",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Sed do eiusmod tempor incididunt",
+      NotetypeID: 2,
+      createdBy: "Bob Johnson",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Ut labore et dolore magna aliqua",
+      NotetypeID: 2,
+      createdBy: "Alice Brown",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Lorem ipsum dolor sit amet",
+      NotetypeID: 1,
+      createdBy: "John Doeed",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Consectetur adipiscing elit",
+      NotetypeID: 2,
+      createdBy: "Jane Smith",
+      createdOn: new Date("May 03, 2023 11:13:00"),
+      modifiedOn: new Date("May 03, 2023 11:13:00"),
+    },
+    {
+      note: "Sed do eiusmod tempor incididunt",
+      NotetypeID: 2,
+      createdBy: "Bob Johnson",
+      createdOn: new Date("May 03, 2023 22:10:00"),
+      modifiedOn: new Date("May 03, 2023 22:10:00"),
+    },
+    {
+      note: "Ut labore et dolore magna aliqua",
+      NotetypeID: 2,
+      createdBy: "Alice Brown",
+      createdOn: new Date("May 04, 2023 22:13:00"),
+      modifiedOn: new Date("May 04, 2023 22:13:00 "),
+    },
+  ];
+  
+  export const Notetype = [
+    {
+      ID: 1,
+      Name: "General Query",
+    },
+    {
+      ID: 2,
+      Name: "ADS Query",
+    },
+    {
+      ID: 3,
+      Name: "Sample Query",
+    },
+    {
+      ID: 4,
+      Name: "ads ",
+    },
+    {
+      ID: 5,
+      Name: "general",
+    },
+  ];
+  
+  export default products;
+  
+  export const actionSheetItems = [{ text: "Delete" }];
+  
+  export const navigation = [
+    { id: 1, text: "Products", icon: "product" },
+    { id: 2, text: "Sales", icon: "money" },
+    { id: 3, text: "Customers", icon: "group" },
+    { id: 4, text: "Employees", icon: "card" },
+    { id: 5, text: "Reports", icon: "chart" },
+  ];
+  
+  export const text =
+    "<h2><b>Drawer Demo</b></h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Penatibus et magnis dis parturient. Eget dolor morbi non arcu risus. Tristique magna sit amet purus gravida quis blandit. Auctor urna nunc id cursus metus aliquam eleifend mi in. Tellus orci ac auctor augue mauris augue neque gravida. Nullam vehicula ipsum a arcu. Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi. Cursus in hac habitasse platea dictumst. Egestas dui id ornare arcu. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.</p><p>Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Neque volutpat ac tincidunt vitae semper quis lectus. Sed sed risus pretium quam vulputate dignissim suspendisse in. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Amet cursus sit amet dictum sit amet justo donec enim. Vestibulum rhoncus est pellentesque elit ullamcorper. Id aliquet risus feugiat in ante metus dictum at.</p>";
+  
