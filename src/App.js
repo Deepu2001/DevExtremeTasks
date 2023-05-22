a771b6ca6917eacf28b3bf9af56e9e01793f7562
deepu agrawal <deepu.agrawal@acsicorp.com>
2023-05-22 18:19:02 +0530

    Define all the routes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import 'devextreme/dist/css/dx.light.css';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DiagnosticCriteria from './components/DiagnosticCriteria';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Sidebar/>}/>
+        <Route path="/popup" element={<DiagnosticCriteria/>}/>
+      </Routes>
+     </Router>
     </div>
   );
 }
